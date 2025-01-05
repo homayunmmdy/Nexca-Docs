98d8d01a23ad6fc06bf98760ae6c796a68f90aaf
Homayoun <homayoun763@gmail.com>
2025-01-05 22:43:11 +0330

    add Docs

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Nexca Docs</span>,
+  project: {
+    link: 'https://github.com/homayunmmdy/Nexca',
+  },
+  docsRepositoryBase: 'https://github.com/homayunmmdy/Nexca-Docs',
+  footer: {
+    text: 'Learn More about Nexca Here',
+  },
+}
+
+export default config
